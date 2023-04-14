c34d96c6c9f64f046fc2f8ac3588c3073970286c
disperse <disperse.recording@gmail.com>
2023-04-13 22:15:09 -0400

    Now highlighting active streamer row in schedule, updated schedule, enabled livelog components.

---
+++ src/data/schedule.js
@@ -0,0 +1,70 @@
+export const data = {
+  "streamers": [
+    {
+      "username": "hackemslashem", "start_time": "1681491600000"
+    },
+    {
+      "username": "Theyflower", "start_time": "1681498800000"
+    },
+    {
+      "username": "PeteGoz", "start_time": "1681506000000"
+    },
+    {
+      "username": "HakureiDeity", "start_time": "1681513200000"
+    },
+    {
+      "username": "JJvanTheMan", "start_time": "1681520400000"
+    },
+    {
+      "username": "Priestess_of_Athe", "start_time": "1681527600000"
+    },
+    {
+      "username": "Cloudmillion", "start_time": "1681534800000"
+    },
+    {
+      "username": "Andrio_Celos", "start_time": "1681542000000"
+    },
+    {
+      "username": "lexi_milch", "start_time": "1681549200000"
+    },
+    {
+      "username": "MajinBird", "start_time": "1681556400000"
+    },
+    {
+      "username": "lidlraccoon", "start_time": "1681563600000"
+    },
+    {
+      "username": "AntiGulp", "start_time": "1681570800000"
+    },
+    {
+      "username": "finitelycraig", "start_time": "1681581600000"
+    },
+    {
+      "username": "luxidream", "start_time": "1681592400000", "notes": "NetHack Bingo competition hosted by luxidream."
+    },
+    {
+      "username": "NabruRocks", "start_time": "1681599600000"
+    },
+    {
+      "username": "spazm9000", "start_time": "1681606800000"
+    },
+    {
+      "username": "jonosmalls88", "start_time": "1681614000000"
+    },
+    {
+      "username": "LythariDrow", "start_time": "1681621200000"
+    },
+    {
+      "username": "Unit327", "start_time": "1681628400000"
+    },
+    {
+      "username": "AmericanElm", "start_time": "1681639200000"
+    },
+    {
+      "username": "DryadMin", "start_time": "1681650000000"
+    },
+    {
+      "username": "d_i_s_p_e_r_s_e", "start_time": "1681660800000"
+    }
+  ]
+};
