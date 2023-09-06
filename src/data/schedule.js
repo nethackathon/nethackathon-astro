fda848d4083aa167094ae1da5919ac9bd2096bbe
disperse <disperse.recording@gmail.com>
2023-09-05 21:31:12 -0400

    Updated schedule, removed sign-up links.

--- src/data/schedule.js
+++ src/data/schedule.js
@@ -1,72 +1,119 @@
 import { DateTime } from "luxon";
-export const startDate = DateTime.fromISO('2023-09-08T21:00:00.000', {zone: 'utc'});
+export const startDate = DateTime.fromISO('2023-09-08T17:00:00.000', {zone: 'utc'});
 export const data = {
   "streamers": [
     {
-      "username": "hackemslashem", "start_time": "1681491600000"
+      "username": "HakureiDeity",
+      "start_time": "1694199600000"
     },
     {
-      "username": "Theyflower", "start_time": "1681498800000"
+      "username": "k2_1971",
+      "start_time": "1694365200000"
     },
     {
-      "username": "PeteGoz", "start_time": "1681506000000"
+      "username": "PeteGoz",
+      "start_time": "1694293200000"
     },
     {
-      "username": "HakureiDeity", "start_time": "1681513200000"
+      "username": "MajinBird",
+      "start_time": "1694257200000"
     },
     {
-      "username": "JJvanTheMan", "start_time": "1681520400000"
+      "username": "spazm9000",
+      "start_time": "1694307600000"
     },
     {
-      "username": "Priestess_of_Athe", "start_time": "1681527600000"
+      "username": "Theyflower",
+      "start_time": "1694192400000"
     },
     {
-      "username": "Cloudmillion", "start_time": "1681534800000"
+      "username": "NabruRocks",
+      "start_time": "1694300400000"
     },
     {
-      "username": "Andrio_Celos", "start_time": "1681542000000"
+      "username": "finitelycraig",
+      "start_time": "1694250000000"
     },
     {
-      "username": "lexi_milch", "start_time": "1681549200000"
+      "username": "lexi_thymi",
+      "start_time": "1694242800000"
     },
     {
-      "username": "MajinBird", "start_time": "1681556400000"
+      "username": "doktor__l",
+      "start_time": "1694264400000"
     },
     {
-      "username": "lidlraccoon", "start_time": "1681563600000"
+      "username": "Cloudmillion",
+      "start_time": "1694329200000"
     },
     {
-      "username": "AntiGulp", "start_time": "1681570800000"
+      "username": "knnn391",
+      "start_time": "1694314800000"
     },
     {
-      "username": "finitelycraig", "start_time": "1681581600000"
+      "username": "AmericanElm",
+      "start_time": "1694271600000"
     },
     {
-      "username": "luxidream", "start_time": "1681592400000", "notes": "NetHack Bingo competition hosted by luxidream."
+      "username": "jonosmalls88",
+      "start_time": "1694235600000"
     },
     {
-      "username": "NabruRocks", "start_time": "1681599600000"
+      "username": "d_i_s_p_e_r_s_e",
+      "start_time": "1694350800000"
     },
     {
-      "username": "spazm9000", "start_time": "1681606800000"
+      "username": "ashpool37",
+      "start_time": "1694358000000"
     },
     {
-      "username": "jonosmalls88", "start_time": "1681614000000"
+      "username": "ChaoticQuakka",
+      "start_time": "1694278800000"
     },
     {
-      "username": "LythariDrow", "start_time": "1681621200000"
+      "username": "DryadMin",
+      "start_time": "1694206800000"
     },
     {
-      "username": "Unit327", "start_time": "1681628400000"
+      "username": "lidlraccoon",
+      "start_time": "1694343600000"
     },
     {
-      "username": "AmericanElm", "start_time": "1681639200000"
+      "username": "Fejana",
+      "start_time": "1694214000000"
     },
     {
-      "username": "DryadMin", "start_time": "1681650000000"
+      "username": "Andrio_Celos",
+      "start_time": "1694322000000"
     },
     {
-      "username": "d_i_s_p_e_r_s_e", "start_time": "1681660800000"
+      "username": "followgraves",
+      "start_time": "1694286000000"
+    },
+    {
+      "username": "JJvanTheMan",
+      "start_time": "1694372400000"
+    },
+    {
+      "username": "Priestess_of_Athe",
+      "start_time": "1694379600000"
+    },
+    {
+      "username": "Unit327",
+      "start_time": "1694221200000"
+    },
+    {
+      "username": "LythariDrow",
+      "start_time": "1694228400000"
+    },
+    {
+      "username": "luxidream",
+      "start_time": "1694386800000",
+      "notes": "NetHack Bingo competition with commentary by world record speedrunner luxidream."
+    },
+    {
+      "username": "aoeixsz4",
+      "start_time": "1694336400000"
     }
   ]
 };
